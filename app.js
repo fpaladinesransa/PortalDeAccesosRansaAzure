0a1a28dfdc7227d40f83429e1e92bb492309e2b7
fpaladinesransa <frearpal@outlook.com>
2023-04-03 12:41:39 -0500

    NewProyect

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(cors({
   origin:"*",
 }))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
